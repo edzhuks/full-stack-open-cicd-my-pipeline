581ad1f3d0056e60962e92ad7c9c911eceb94ba1
edgars <edzhuks@gmail.com>
2023-04-18 22:26:28 +0300

    part11-ex20 tenth try

--- cypress/e2e/phonebook.cy.js
+++ cypress/e2e/phonebook.cy.js
@@ -2,6 +2,6 @@ describe('Phonebook', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:3003')
     cy.contains('Phonebook')
-    cy.contains('Filter shown with')
+    cy.contains('filter shown with')
   })
 })
